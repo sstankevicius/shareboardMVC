ebec340659f4e8f29041ea82763ace81cf83b083
Simonas <stankeviciussimonas@gmail.com>
2018-08-14 00:26:45 +0300

    first commit

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+// Define DB Params
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "root");
+define("DB_NAME", "shop");
+
+// Define URL
+define("ROOT_PATH", "/oopmvcUdemy/");
+define("ROOT_URL", "http://localhost:8888/oopmvcUdemy");
